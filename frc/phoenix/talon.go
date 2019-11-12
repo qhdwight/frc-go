9fa4dedc735eac9d8262707a8c89a168e29f6fac
Quintin Dwight <qhdwight@gmail.com>
2019-11-11 23:15:15 -0800

    Removed unecessary cgo flags

--- frc/phoenix/talon.go
+++ frc/phoenix/talon.go
@@ -1,6 +1,6 @@
 package phoenix
 
-// #cgo CFLAGS: -I${SRCDIR}/include -I/Users/quintin/.gradle/toolchains/frc/2019/roborio/arm-frc2019-linux-gnueabi/usr/include/c++/7.3.0 -I/Users/quintin/.gradle/toolchains/frc/2019/roborio/arm-frc2019-linux-gnueabi/usr/include/c++/7.3.0/arm-frc2019-linux-gnueabi
+// #cgo CFLAGS: -I${SRCDIR}/include
 // #cgo CXXFLAGS: -I${SRCDIR}/include
 // #cgo LDFLAGS: -L${SRCDIR}/../lib/athena -lwpiHal -lwpiutil -lstdc++ -lm -lFRC_NetworkCommunication -lNiFpga -lNiFpgaLv -lniriodevenum -lniriosession -lNiRioSrv -lRoboRIO_FRC_ChipObject -lvisa -Llib/athena -lCTRE_Phoenix -lCTRE_PhoenixCCI
 // #include "phoenix.h"
--- frc/robot.go
+++ frc/robot.go
@@ -1,7 +1,6 @@
 package frc
 
-// #cgo CFLAGS: -I${SRCDIR}/include -I/Users/quintin/.gradle/toolchains/frc/2019/roborio/arm-frc2019-linux-gnueabi/usr/include/c++/7.3.0 -I/Users/quintin/.gradle/toolchains/frc/2019/roborio/arm-frc2019-linux-gnueabi/usr/include/c++/7.3.0/arm-frc2019-linux-gnueabi
-// #cgo CXXFLAGS: -I${SRCDIR}/include
+// #cgo CFLAGS: -I${SRCDIR}/include
 // #cgo LDFLAGS: -L${SRCDIR}/lib/athena -lwpiHal -lwpiutil -lstdc++ -lm -lFRC_NetworkCommunication -lNiFpga -lNiFpgaLv -lniriodevenum -lniriosession -lNiRioSrv -lRoboRIO_FRC_ChipObject -lvisa
 // #include "hal.h"
 import "C"
